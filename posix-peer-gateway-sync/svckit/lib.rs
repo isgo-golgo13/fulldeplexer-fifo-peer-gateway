de5e9295a6ce11c81500e0d9050985bb2d40e1ff
Tomas Wallick <wallick.tom@gmail.com>
2024-09-10 12:00:10 +0200

    updated lib.rs

---
+++ posix-peer-gateway-sync/svckit/lib.rs
@@ -0,0 +1,3 @@
+pub mod fullduplex_gateway;
+pub mod fullduplex_peer; // FullDuplexPeer struct and implementation
+pub mod fullduplexer; // FullDuplexer trait // FullDuplexPeerGateway struct and implementation
